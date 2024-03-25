f4489f47232fe5c2c5f74cf6a3604f3b45acfb3a
gnaak <139318007+gnaak@users.noreply.github.com>
2024-03-25 21:23:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.52 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.9 MB, 시간: 6.23 ms
+메모리: 38.1 MB, 시간: 9.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 19:08:32
+2024년 03월 25일 21:23:52
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,29 +1,66 @@
+class Queue {
+  constructor() {
+
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+
+  getSize() {
+    return this.items.length;
+  }
+
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+}
+
 function solution(maps) {
-    const rows = maps.length
-    const cols = maps[0].length
-    const dx = [1,0,-1,0]
-    const dy = [0,1,0,-1]
-    
-    const queue = [[0,0,1]]
-    // 시작 지점 표시
-    maps[0][0] = 0 
-    
-    while (queue.length > 0){
-        const [x, y , dist] = queue.shift()
-        
-        if(x===cols-1 && y===rows-1){
-            return dist
-        }
-        
-        for (let i = 0; i<4;i++){
-            const nx = x + dx[i]
-            const ny = y + dy[i]
-            
-            if (nx >=0 && ny>= 0 && nx <cols && ny < rows && maps[ny][nx] ===1){
-                queue.push([nx, ny, dist +1])
-                maps[ny][nx] = 0
+
+    const dx=[0,1,0,-1];
+    const dy=[1,0,-1,0];
+    const width=maps[0].length;
+    const height=maps.length;
+    const visit=Array.from({length:height},()=>Array(width).fill(0));
+    function bfs(){
+        const queue=new Queue();
+        queue.enqueue([0,0]);
+
+        while(!queue.isEmpty()){
+
+            const [x,y]= queue.dequeue();
+
+
+
+            if((x===height-1)&&(y===width-1))return visit[x][y]+1;
+            for(let a=0;a<4;a++){
+             let newX=x+dx[a];
+             let newY=y+dy[a]; 
+
+                if (newX < 0 || newY < 0 || newX >= height || newY >= width || maps[newX] === undefined || maps[newX][newY] === undefined || maps[newX][newY] === 0) {
+    continue;
+}
+                if(visit[newX][newY]===0){
+                     visit[newX][newY]=visit[x][y]+1;
+              //      console.log('newX newY',newX,newY);
+             queue.enqueue([newX,newY]);  
+                }
+
+
             }
+
         }
+        return -1;
     }
-    return -1 
+    return bfs();
 }
