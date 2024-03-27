22bda57f4effaca0f8c4f40890fd349dbcb26852
gnaak <139318007+gnaak@users.noreply.github.com>
2024-03-27 23:16:20 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 1428 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -1,32 +1,26 @@
-'''
-백준 골드 4
-가까운 조상 찾기
-'''
-
-tc= int(input())
+tc = int(input())
 for _ in range(tc):
     n = int(input())
-    parent_node = [0 for _ in range(n+1)] # 부모 노드 저장용
+    parent_node = [0 for i in range(n+1)]
     for i in range(n-1):
         p, c = map(int,input().split())
-        parent_node[c] = p  #
+        parent_node[c] = p
 
     a, b = map(int,input().split())
-    a_parent = [a]
-    b_parent = [b]
+    a_anc = [a]
+    b_anc = [b]
 
     while parent_node[a]:
-        a_parent.append(parent_node[a])
-        a=parent_node[a]
+        a = parent_node[a]
+        a_anc.append(a)
 
     while parent_node[b]:
-        b_parent.append(parent_node[b])
-        b=parent_node[b]
+        b = parent_node[b]
+        b_anc.append(b)
+    a_lv = len(a_anc)-1
+    b_lv = len(b_anc)-1
+    while a_anc[a_lv] == b_anc[b_lv]:
+        a_lv -=1
+        b_lv -=1
 
-    a_level = len(a_parent)-1
-    b_level = len(b_parent)-1
-
-    while a_parent[a_level] == b_parent[b_level]:
-        a_level -=1
-        b_level -=1
-    print(a_parent[a_level+1])
+    print(a_anc[a_lv+1])
