6dac73c103246c6972d534d22be269f5f5a3c29a
gnaak <139318007+gnaak@users.noreply.github.com>
2024-03-27 22:54:02 +0900

    [Gold V] Title: 문자 해독, Time: 1644 ms, Memory: 36980 KB -BaekjoonHub

--- 백준/Gold/1593. 문자 해독/문자 해독.py
+++ 백준/Gold/1593. 문자 해독/문자 해독.py
@@ -1,37 +1,24 @@
-'''
-백준 골드 5
-문자 해독
-'''
-
-g, s = map(int,input().split()) # 단어 W의 길이 g, 추출한 문자열 S의 길이 s
-W = str(input())
+g, s = map(int,input().split())
+w = str(input())
 S = str(input())
-wl = [0]*52
-sl = [0]*52
 
-for i in range(g):
-    if ord(W[i]) >= 97:
-        wl[ord(W[i])-71] +=1
-    else:
-        wl[ord(W[i])-65] +=1
+w_l = [0]*58
+s_l = [0]*58
+start= 0
+length= 0
 cnt = 0
-start = 0
-length = 0
+for i in range(g):
+    w_l[ord(w[i])-65] +=1
 
 for i in range(s):
-    if ord(S[i]) >= 97:
-        sl[ord(S[i])-71] +=1
-    else:
-        sl[ord(S[i])-65] +=1
+    s_l[ord(S[i])-65] +=1
     length +=1
-    if length == g :
-        if wl == sl:
+    if length == g:
+        if w_l == s_l:
             cnt +=1
-        if ord(S[start]) >= 97:  # 소문자
-            sl[ord(S[start]) - 97 + 26] -= 1
-        elif ord(S[start]) >= 65:  # 대문자
-            sl[ord(S[start]) - 65] -= 1
+
+        s_l[ord(S[start])-65]-=1
         start+=1
         length-=1
-
 print(cnt)
+
