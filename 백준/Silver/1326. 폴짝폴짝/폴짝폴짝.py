6439d29b5bb8d2aa65bdac827eeb21dbc601fa93
gnaak <139318007+gnaak@users.noreply.github.com>
2024-03-31 12:16:06 +0900

    [Silver II] Title: 폴짝폴짝, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
@@ -1,4 +1,3 @@
-
 def bfs():
     queue = [a-1]
     visited = [-1] * n
@@ -14,17 +13,17 @@ def bfs():
                 if i == b - 1:
                     return visited[i]
 
-        for i in range(current, -1, -node[current]):
+        for i in range(current,-1,-node[current]):
             if visited[i] == -1:
                 queue.append(i)
                 visited[i] = visited[current] + 1
-                if i == b - 1:
+
+                if i == b-1:
                     return visited[i]
 
     return -1
-
-
 n = int(input())
 node = list(map(int,input().split()))
 a , b = map(int,input().split())
+
 print(bfs())
