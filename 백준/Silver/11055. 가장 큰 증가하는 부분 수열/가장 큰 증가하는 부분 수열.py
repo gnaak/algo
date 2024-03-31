4d4c20a2b6d5eff58def000e6306b031e370d4d1
gnaak <139318007+gnaak@users.noreply.github.com>
2024-03-31 22:28:30 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -3,16 +3,15 @@
 가장 큰 증가하는 부분 순열
 '''
 
-n=int(input())
-array=list(map(int, input().split()))
-
-d=[1]*n
-d[0]=array[0]
+n = int(input())
+arr = list(map(int,input().split()))
+dp = [0]*n
+dp[0] = arr[0]
 for i in range(1,n):
-  for j in range(i):
-    if array[j]<array[i]:
-      d[i]=max(d[i], d[j]+array[i])
-    else:
-      d[i]=max(d[i], array[i])
+    for j in range(i):
+        if arr[j] < arr[i] :
+            dp[i] = max(dp[j]+arr[i], dp[i])
+        else:
+            dp[i] = max(dp[i],arr[i])
 
-print(max(d))
+print(max(dp))
