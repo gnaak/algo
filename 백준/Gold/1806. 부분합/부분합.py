5d475b60f30571ac82410b4e1d2c7ca8a2d03e3b
gnaak <139318007+gnaak@users.noreply.github.com>
2024-04-15 00:09:24 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,28 +1,22 @@
-'''
-백준 골드 4
-부분합
-'''
-
 import sys
 
-n, s = map(int,input().split()) # n길이의 수열, 목표 합 s
-nums = list(map(int,input().split()))
-ans = 0
-start = end = 0
+n, s = map(int, input().split())  # n길이의 수열, 목표 합 s
+nums = list(map(int, input().split()))
+ans = nums[0]
+start = 0
+end = 1
 cnt = sys.maxsize
 
 while True:
     if ans >= s:
-        cnt = min(cnt, end-start)
+        cnt = min(cnt, end - start)
         ans -= nums[start]
-        start +=1
-
+        start += 1
     elif end == n:
         break
-
     else:
-        ans+=nums[end]
-        end +=1
+        ans += nums[end]
+        end += 1
 
 if cnt == sys.maxsize:
     print(0)
