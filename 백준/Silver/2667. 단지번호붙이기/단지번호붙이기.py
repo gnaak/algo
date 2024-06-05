471e271e72461a01dd72884e9ffae53daf65becf
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-05 14:35:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,24 +1,38 @@
-n = int(input())
-maps = [list(map(int,input())) for _ in range(n)]
-cnt_ls = []
-for i in range(n):
-    for j in range(n):
-        if maps[i][j] == 1:
-            cnt = 1
-            q = []
-            q.append((i,j))
-            maps[i][j] = 0
-            while q:
-                k, l = q.pop(0)
-                for di, dj in [[1,0],[0,1],[0,-1],[-1,0]]:
-                    ni, nj = k+di, l+dj
-                    if 0<=ni<n and 0<=nj<n and maps[ni][nj]==1:
-                        cnt += 1
-                        q.append((ni,nj))
-                        maps[ni][nj] = 0
-            if cnt:
-                cnt_ls.append(cnt)
-cnt_ls.sort()
-print(len(cnt_ls))
-for i in range(len(cnt_ls)):
-    print(cnt_ls[i])
+'''
+백준 실버 1
+단지번호붙이기
+'''
+
+
+def bfs(i,j):
+    queue = [(i,j)]
+    nums = 1
+    while queue:
+        y, x = queue.pop(0)
+        for k in range(4):
+            ny = y +dy[k]
+            nx = x +dx[k]
+            if 0<=nx<n and 0<=ny<n and board[ny][nx] == 1:
+                queue.append((ny,nx))
+                nums +=1
+                board[ny][nx] = 0
+    return nums
+
+n = int(input()) # 지도의 가로, 세로 크기
+board = [list(map(int,input())) for _ in range(n)]
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+cnt = 0
+num_lis = []
+for i in range(n): # 세로 순회
+    for j in range(n): # 가로 순회
+        if board[i][j] == 1:
+            board[i][j] = 0
+            cnt += 1
+            num_lis.append(bfs(i,j))
+num_lis.sort()
+print(cnt)
+for i in range(len(num_lis)):
+    print(num_lis[i])
