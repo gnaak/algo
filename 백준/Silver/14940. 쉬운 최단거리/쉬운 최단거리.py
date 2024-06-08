f2628b3ca0391a510582e149411a5121b966cd1b
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-08 22:20:21 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 516 ms, Memory: 39720 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def bfs(start_y, start_x):
+    queue = deque([(start_y, start_x, 0)])
+    while queue:
+        y, x, cnt = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == 0:
+                visited[ny][nx] = 1
+                if board[ny][nx] == 1:
+                    board[ny][nx] = cnt + 1
+                    queue.append((ny, nx, cnt + 1))
+
+n, m = map(int, input().split())  # 지도 크기 n, m
+board = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+start_y, start_x = -1, -1
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 2:
+            start_y, start_x = i, j
+            break
+    if start_y != -1:
+        break
+
+visited[start_y][start_x] = 1
+board[start_y][start_x] = 0
+bfs(start_y, start_x)
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 1 and visited[i][j] == 0:
+            board[i][j] = -1
+
+for row in board:
+    print(*row)
