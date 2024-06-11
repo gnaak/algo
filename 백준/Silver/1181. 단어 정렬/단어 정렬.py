7f463a706fcbc990392c3734fd5385351538bf19
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-11 22:47:35 +0900

    [Silver V] Title: 단어 정렬, Time: 808 ms, Memory: 35732 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,18 @@
+'''
+백준 실버 5
+단어 정렬
+'''
+
+
+n = int(input())
+lst = []
+
+for i in range(n):
+    lst.append(input())
+set_lst = set(lst)	## set으로 변환해서 중복값을 제거
+lst = list(set_lst)	## 다시 list로 변환
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
