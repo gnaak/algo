050cbdc08782665b4014df692540170d374b77e2
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-27 22:42:31 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.98 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,35 +1,27 @@
 def solution(friends, gifts):
-    gift_s = {friend : [] for friend in friends}
-    gift_r = {friend : 0 for friend in friends}
-    for gift in gifts:
-        s, r = gift.split()
-        gift_s[s] = []
-
-    for gift in gifts:
-        s, r = gift.split()
-        gift_r[s] += 1
-        gift_r[r] -= 1
-        gift_s[s].append(r)
-
     n = len(friends)
-    graph = [[0]*(n) for _ in range(n)]
-    hashed_friend = {friends[i]: i for i in range(n)} 
+    gift_lst = {}
+    gift_zisu = {friend : 0 for friend in friends}
+    for i in range(n):
+        gift_lst[friends[i]] = i
     
-    # 그래프 만들기 
-    for sender in gift_s:
-        for reciever in gift_s[sender]:
-            graph[hashed_friend[sender]][hashed_friend[reciever]] += 1 
+    graph =[[0]*n for _ in range(n)]
     
-    answer = 0
-    gift = [0]*n
+    for gift in gifts:
+        send, recieve = gift.split(" ")
+        graph[gift_lst[send]][gift_lst[recieve]]+=1
+        gift_zisu[send] += 1
+        gift_zisu[recieve] -= 1
+    answer = [0]*n
     for i in range(n):
         for j in range(n):
-            if i != j:
-                if graph[i][j] != graph[j][i]:
-                    if graph[i][j] > graph[j][i]:
-                        gift[i] += 1
-                else: 
-                    if gift_r[friends[i]] != gift_r[friends[j]]:
-                        if gift_r[friends[i]] > gift_r[friends[j]]: 
-                            gift[i] += 1
-    return max(gift)
+            if i != j :
+                if graph[i][j] > graph[j][i]:
+                    answer[i] +=1 
+                    
+                elif graph[i][j] == graph[j][i]:
+                    if gift_zisu[friends[i]] > gift_zisu[friends[j]]:
+                        answer[i] += 1
+
+    return max(answer)
+                    
