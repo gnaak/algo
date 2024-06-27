60eaf98fc5fb109c92c55a6d87390c50ec15fece
이근학 <139318007+gnaak@users.noreply.github.com>
2024-06-28 00:24:55 +0900

    [level 2] Title: 타겟 넘버, Time: 1.04 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.77 ms
+메모리: 35.4 MB, 시간: 1.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 07:14:55
+2024년 06월 28일 00:24:54
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,38 +1,15 @@
-// DFS로 풀자!
-
-// [+1, +1, +1, +1, +1]
-// [+1, +1, +1, +1, -1]
-// [+1, +1, +1, -1, +1]
-// [+1, +1, +1, -1, -1]
-// [+1, +1, -1, +1, +1]
-// [+1, +1, -1, +1, -1]
-// [+1, +1, -1, -1, +1]
-// [+1, +1, -1, -1, -1]
-
-// 1. 이번턴에 어떤 동작을 수행할 것인가?
-// 2. 언제 탈출할 것인가?
-// 3. 누적합이 target과 같다면 return
-
 function solution(numbers, target) {
-    let answer = 0;
-
-    function DFS(sum, depth) {
-        // 2)
-        if(depth == numbers.length) {
-            // 3)
-            if(sum == target) {
-                answer += 1;
-            }
-
-            return;
-        }
-
-        // 1)
-        DFS(sum + numbers[depth], depth+1);
-        DFS(sum - numbers[depth], depth+1);
+    
+    function dfs(sum, depth){
+        if (depth == numbers.length){
+            if (sum == target) answer+=1;
+            return
+        } 
+        dfs(sum+numbers[depth], depth+1)        
+        dfs(sum-numbers[depth], depth+1)        
     }
-
-    DFS(0, 0);
-
+    
+    var answer = 0;
+    dfs(0, 0)
     return answer;
 }
