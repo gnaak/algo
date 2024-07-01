bf709f3a931d05b931dbd566a69004956ea8a247
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-02 08:03:08 +0900

    [Gold IV] Title: 호텔, Time: 108 ms, Memory: 10792 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.js
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -1,20 +1,15 @@
-const fs = require('fs'); 
-const input = fs.readFileSync('/dev/stdin').toString().trim()
-  .split("\n")
-  .map((el) => el.split(" ").map(Number));
-const [C, N] = input.shift();
-
-const dp = new Array(C + 1).fill(Infinity);
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [c, n] = input[0].split(" ").map(Number); // 늘려야 하는 고객 c, 홍보할 수 있는 도시 n
+let dp = Array(c + 1).fill(Infinity); // 도시당 비용 100보다 작거나 같은 자연수 ; 드는 비용
 dp[0] = 0;
 
-for (let [cost, people] of input) {
-  if (dp[people] > cost) dp[people] = cost;
-  for (let i = 1; i <= C; i++) {
-    dp[i] =
-      i < people
-        ? Math.min(dp[i], cost)
-        : Math.min(dp[i], dp[people] + dp[i - people]);
+for (let i = 1; i <= n; i++) {
+  const [a, b] = input[i].split(" ").map(Number); // 비용 a, 증가하는 고객의 수 b
+  if (dp[b] > a) dp[b] = a;
+  for (let j = 1; j <= c; j++) {
+    dp[j] = j < b ? Math.min(dp[j], a) : Math.min(dp[j], dp[b] + dp[j - b]);
   }
 }
 
-console.log(dp[C]);
+console.log(dp[c]);
