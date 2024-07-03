b3a85f94f15bf4e7ba9e8b5abdd698cc4045e5b5
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-04 08:49:35 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/README.md
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 흉부외과 또는 일반외과 의사 목록 출력하기 - 132203 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132203#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132203) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 28일 00:11:23
+2024년 07월 04일 08:49:33
 
 ### 문제 설명
 
--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,7 +1,5 @@
 -- 코드를 입력하세요
-
-
--- 코드를 입력하세요
-SELECT DR_NAME, DR_ID, MCDP_CD, date_format(hire_ymd, "%Y-%m-%d") as HIRE_YMD from doctor
-where MCDP_CD = "cs" or MCDP_CD = "gs"
-order by hire_ymd desc, dr_name asc
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD,'%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR 
+WHERE MCDP_CD IN ('CS','GS')
+ORDER BY HIRE_YMD DESC, DR_NAME
