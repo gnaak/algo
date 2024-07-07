f4c16dcecb1a2bde4d54feeef703c2644019cbec
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-07 19:46:51 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,22 +1,31 @@
 function solution(n, computers) {
-    const visited = []
-    for (let i=0; i<n;i++){
-        visited.push(0)
-    }
-    function dfs(current){
-        visited[current] = 1;
-        for (let i = 0; i<n; i++){
-            if(computers[current][i] == 1 && visited[i] == 0){
-                dfs(i)
-            }           
+    let visited = Array.from({ length: n }, () => 0);
+
+    function bfs(start) {
+        let q = [];
+        q.push(start);
+        visited[start] = 1;
+
+        while (q.length > 0) {
+            let now = q.shift();
+            for (let next = 0; next < n; next++) {
+                if (computers[now][next] === 1 && visited[next] === 0) {
+                    visited[next] = 1;
+                    q.push(next);
+                }
+            }
         }
     }
+
     let answer = 0;
-    for (let i = 0 ; i<n;i++){
-        if (visited[i] == 0){
-            dfs(i);
-            answer ++
+    for (let i = 0; i < n; i++) {
+        if (visited[i] === 0) {
+            bfs(i);
+            answer++;
         }
     }
+
     return answer;
 }
+
+
