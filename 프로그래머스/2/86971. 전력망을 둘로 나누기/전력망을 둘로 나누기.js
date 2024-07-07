b06fdcfdd3e9100fec860292c55336d2821bf1ee
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-07 19:29:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.23 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,9 +1,9 @@
 function solution(n, wires) {
-  function bfs(graph, idx, cnt) {
+  function bfs(graph, cnt) {
     q = [];
     visited = Array(n).fill(0);
-    q.push(idx);
-    visited[idx] = 1;
+    q.push(0);
+    visited[0] = 1;
 
     while (q.length) {
       s = q.shift();
@@ -21,6 +21,7 @@ function solution(n, wires) {
   function makeGraph(n, wire) {
     let graph = Array.from({ length: n }, () => []); // 배열을 n개의 빈 배열로 초기화
     for ([s, e] of wire) {
+      // 양방향 그래프이므로 양쪽에 다 표시해줌 
       graph[s - 1].push(e - 1);
       graph[e - 1].push(s - 1);
     }
@@ -30,8 +31,12 @@ function solution(n, wires) {
   let answer = n;
 
   for (let i = 0; i < n; i++) {
+    // 1. 전선을 하나씩 자르기
     let wire = [...wires.slice(0, i), ...wires.slice(i + 1)];
-    let one = bfs(makeGraph(n, wire), i, 1);
+    // 2. 자른 배열로 그래프 만들기 
+    let graph = makeGraph(n, wire)
+    // 3. 만든 그래프로 bfs 돌리기 
+    let one = bfs(graph,1)
     answer = Math.min(answer, Math.abs(n - one - one));
   }
 
