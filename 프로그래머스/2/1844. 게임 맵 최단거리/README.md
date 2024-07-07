3969247121c0e469f6ac1b9cc7512abdd81c1c19
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-07 20:18:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.71 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 5.14 ms
+메모리: 38 MB, 시간: 6.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 07:05:19
+2024년 07월 07일 20:18:52
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,66 +1,39 @@
-class Queue {
-  constructor() {
-
-    this.items = [];
-  }
-
-  enqueue(item) {
-    this.items.push(item);
-  }
-
-  dequeue() {
-    return this.items.shift();
-  }
-
-  peek() {
-    return this.items[0];
-  }
-
-  getSize() {
-    return this.items.length;
-  }
-
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-}
-
 function solution(maps) {
+  const dx = [1, 0, -1, 0];
+  const dy = [0, 1, 0, -1];
+  const n = maps.length;
+  const m = maps[0].length;
 
-    const dx=[0,1,0,-1];
-    const dy=[1,0,-1,0];
-    const width=maps[0].length;
-    const height=maps.length;
-    const visit=Array.from({length:height},()=>Array(width).fill(0));
-    function bfs(){
-        const queue=new Queue();
-        queue.enqueue([0,0]);
+  function bfs() {
+    let q = [];
+    q.push([0, 0]);
 
-        while(!queue.isEmpty()){
+    while (q.length) {
+      const [y, x] = q.shift();
 
-            const [x,y]= queue.dequeue();
+      // 목적지에 도달했을 때 거리 반환
+      if (y === n - 1 && x === m - 1) {
+        return maps[y][x];
+      }
 
+      for (let i = 0; i < 4; i++) {
+        let ny = y + dy[i];
+        let nx = x + dx[i];
 
-
-            if((x===height-1)&&(y===width-1))return visit[x][y]+1;
-            for(let a=0;a<4;a++){
-             let newX=x+dx[a];
-             let newY=y+dy[a]; 
-
-                if (newX < 0 || newY < 0 || newX >= height || newY >= width || maps[newX] === undefined || maps[newX][newY] === undefined || maps[newX][newY] === 0) {
-    continue;
-}
-                if(visit[newX][newY]===0){
-                     visit[newX][newY]=visit[x][y]+1;
-              //      console.log('newX newY',newX,newY);
-             queue.enqueue([newX,newY]);  
-                }
-
-
-            }
-
+        // 범위 확인
+        if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
+          // 방문하지 않았고 이동 가능한 경우
+          if (maps[ny][nx] === 1) {
+            q.push([ny, nx]);
+            maps[ny][nx] = maps[y][x] + 1; // 이동 거리 계산하여 저장
+          }
         }
-        return -1;
+      }
     }
-    return bfs();
+
+    return -1; // 도달할 수 없는 경우
+  }
+
+  let answer = bfs();
+  return answer;
 }
