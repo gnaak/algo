9d0fb898724abc7f0f1836c0a0f0150540d523ac
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-08 14:55:44 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 21192 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const f = (depth) => {
+  if (depth == m) {
+    output += tmp.join(' ')
+    output += '\n'
+    return
+  }
+
+  for (let i = 0; i < n; i++){
+    if (!check[i]) {
+      tmp.push(arr[i])
+      check[i] = 1
+      f(depth + 1)
+      check[i] = 0
+      tmp.pop()
+    }
+  }
+  
+}
+
+
+const [n, m] = input[0].split(' ').map(Number)
+const arr = input[1].split(' ').map(Number)
+arr.sort((a,b)=>a-b)
+let tmp = []
+let check = Array(n).fill(0)
+let output = ''
+f(0)
+console.log(output)
