5deac639d1452c7656838c9e4a3aa8461f7507c3
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-08 14:26:02 +0900

    [Silver III] Title: N과 M (4), Time: 184 ms, Memory: 12176 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.js
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -1,23 +1,24 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [a, b] = input[0].split(" ").map(Number);
-const arr = Array.from({ length: a }, (_, i) => i + 1);
-const tmp = Array.from({ length: b }, () => 0);
-
-const f = (num, depth) => {
-  if (depth === b) {
-    console.log(tmp.join(' '));
+const f = (depth) => {
+  if (depth == m) {
+    output += tmp.join(" ");
+    output += "\n";
     return;
-  }
-
-  for (let k of arr) {
-    tmp[depth] = k;
-    if (depth === 0 || tmp[depth] >= tmp[depth - 1]) {
-      f(num, depth + 1);
+  } else {
+    for (let i = 1; i <= n; i++) {
+      if (depth == 0 || tmp[depth - 1] <= i) {
+        tmp.push(i);
+        f(depth + 1);
+        tmp.pop();
+      }
     }
-    tmp[depth] = 0;
   }
 };
 
-f(a, 0);
+const [n, m] = input[0].split(" ");
+let output = "";
+let tmp = [];
+f(0);
+console.log(output);
