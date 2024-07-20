f809b4d1638a2984b4946ed14251bec15a0567ad
이근학 <139318007+gnaak@users.noreply.github.com>
2024-07-20 19:43:58 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1084 ms, Memory: 146836 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.js
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.js
@@ -2,18 +2,24 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const f = () => {
+  // mid 값을 구하기 위해서 가장 짧은 길이와 가장 긴 길이
   let min = 1;
   let max = c[n - 1];
 
   while (min <= max) {
     let mid = Math.floor((min + max) / 2);
-
-    let cnt = c.reduce((a, c) => a + Math.floor(c / mid), 0);
-
-    if (cnt >= m) min = mid + 1;
-    else max = mid - 1;
+    // 각각의 과자 길이를 중간값으로 나누었을 때, cnt는 나누어 줄 수 있는 사람의 수
+    let cnt = 0 
+    for(let i = 0; i < n; i++) {
+      cnt += Math.floor(c[i] / mid);
+    }    // cnt가 m보다 크거나 같으면, 길이를 더 늘려서 나누어줘도 가능함
+    if (cnt >= m) {
+      min = mid + 1;
+    } else {
+      max = mid - 1;
+    }
   }
-    console.log(max)
+  console.log(max);
 };
 
 const [m, n] = input[0].split(" ").map(Number); // m은 조카의 수, n은 과자의 수
