049cc48184f63dc08be86a7712a863b958cc345b
이근학 <139318007+gnaak@users.noreply.github.com>
2024-10-09 22:45:41 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,23 @@
-n = int(input()) # 컴퓨터의 수
-m = int(input()) # 연결되어 있는 컴퓨터의 수
-com = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b = map(int,input().split())
-    com[a].append(b)
-    com[b].append(a)
+import sys
+input = sys.stdin.readline
+n = int(input()) # 컴퓨터의 숫자
 
-infected = [0]*(n+1)
+
+network =[[] for _ in range(n+1)]
+m = int(input()) # 컴퓨터들이 연결되어 있는 숫자
+for _ in range(m):
+    s, e = map(int,input().split())
+    network[s].append(e)
+    network[e].append(s)
 queue = [1]
+infected = [0] * (n+1)
+infected[1] = 1
 while queue:
-    vir = queue.pop(0)
-    infected[vir] = 1
-    for nxt in com[vir]:
-        if infected[nxt] == 0:
-            queue.append(nxt)
+    current = queue.pop(0)
+    infected[current] = 1
+
+    for nxt_computer in network[current]:
+        if infected[nxt_computer] == 0 :
+            queue.append(nxt_computer)
 
 print(sum(infected)-1)
