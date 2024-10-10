3581c2085b4d1aa625b434fdf0f0e9e32ddcd26d
이근학 <139318007+gnaak@users.noreply.github.com>
2024-10-10 19:28:21 +0900

    [Silver V] Title: 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,13 +1,24 @@
+# 0과 1로 이루어진 문자열 S
+# 다솜이가 할 수 있는 행동은 S에서 연속된 하나 이상의 수를 모두 뒤집는 것
+
 import sys
 input = sys.stdin.readline
 
-n = str(input().strip())
-cnt = [0,0]
-cnt[int(n[0])] += 1
-for i in range(1,len(n)):
-    if n[i] != n[i-1]:
-        cnt[int(n[i])]+=1
-    elif i == len(n):
-        cnt[int(n[-1])] +=1
+zeros = 0
+ones = 0
+s = input().strip()
 
-print(min(cnt))
+for i in range(len(s)):
+    if i == 0 :
+        if s[i] == '0':
+            zeros += 1
+        else:
+            ones += 1
+    else:
+        if s[i] != s[i-1]:
+            if s[i] == '0':
+                zeros += 1
+            else:
+                ones += 1
+
+print(min(ones, zeros))
