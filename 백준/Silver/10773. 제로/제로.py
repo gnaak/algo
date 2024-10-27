57c11f9c4f983eb5bc8421b0bf6ea13efbbd5000
이근학 <139318007+gnaak@users.noreply.github.com>
2024-10-28 00:04:35 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+# 제로
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+for _ in range(n):
+    a = int(input())
+    if a != 0 : stack.append(a)
+    else: stack.pop()
+
+print(sum(stack))
