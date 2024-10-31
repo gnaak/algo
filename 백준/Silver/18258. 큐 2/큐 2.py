d035b26cb16b153a450dffe2fd7de707b13da10c
이근학 <139318007+gnaak@users.noreply.github.com>
2024-10-31 23:43:36 +0900

    [Silver IV] Title: 큐 2, Time: 1832 ms, Memory: 167528 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -6,24 +6,27 @@ queue = deque([])
 n = int(input())
 result = []
 
-for _ in range(n):
-    command_lst = list(map(str, input().split()))
-    command = command_lst[0]
-    if command == 'push':
-        queue.append(int(command_lst[1]))
-    elif command == 'pop':
-        if queue:
-            result.append(queue.popleft())
+def main():
+    for _ in range(n):
+        command_lst = list(map(str, input().split()))
+        command = command_lst[0]
+        if command == 'push':
+            queue.append(int(command_lst[1]))
+        elif command == 'pop':
+            if queue:
+                result.append(queue.popleft())
+            else:
+                result.append(-1)
+        elif command == 'size':
+            result.append(len(queue))
+        elif command == 'empty':
+            result.append(0 if queue else 1)
+        elif command == 'front':
+            result.append(queue[0] if queue else -1)
         else:
-            result.append(-1)
-    elif command == 'size':
-        result.append(len(queue))
-    elif command == 'empty':
-        result.append(0 if queue else 1)
-    elif command == 'front':
-        result.append(queue[0] if queue else -1)
-    else:  
-        result.append(queue[-1] if queue else -1)
+            result.append(queue[-1] if queue else -1)
+    
+    # 결과 한 번에 출력
+    print('\n'.join(map(str,result)))
 
-# 결과 한 번에 출력
-print('\n'.join(map(str,result)))
+main()
