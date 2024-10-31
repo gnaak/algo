1bcb215ea9525d37294523d414427540e4e5b7b2
이근학 <139318007+gnaak@users.noreply.github.com>
2024-10-31 23:42:53 +0900

    [Silver IV] Title: 큐 2, Time: 2204 ms, Memory: 167508 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,34 +1,29 @@
-# 큐 2
-
 import sys
 input = sys.stdin.readline
 from collections import deque
+
 queue = deque([])
 n = int(input())
+result = []
+
 for _ in range(n):
-    command_lst = list(map(str,input().split()))
+    command_lst = list(map(str, input().split()))
     command = command_lst[0]
     if command == 'push':
         queue.append(int(command_lst[1]))
     elif command == 'pop':
         if queue:
-            print(queue.popleft())
+            result.append(queue.popleft())
         else:
-            print(-1)
+            result.append(-1)
     elif command == 'size':
-        print(len(queue))
+        result.append(len(queue))
     elif command == 'empty':
-        if queue:
-            print(0)
-        else:
-            print(1)
+        result.append(0 if queue else 1)
     elif command == 'front':
-        if queue:
-            print(queue[0])
-        else:
-            print(-1)
-    else:
-        if queue:
-            print(queue[-1])
-        else:
-            print(-1)
+        result.append(queue[0] if queue else -1)
+    else:  
+        result.append(queue[-1] if queue else -1)
+
+# 결과 한 번에 출력
+print('\n'.join(map(str,result)))
