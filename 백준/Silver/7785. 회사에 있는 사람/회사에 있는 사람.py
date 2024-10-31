cabaf348cab76a2af4f7d4916113c41cebf76eb3
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-01 01:53:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 168 ms, Memory: 42188 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,19 @@
+# 회사에 있는 사람 
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+employee = dict()
+
+for _ in range(n):
+    name, act = input().split()
+    if act == 'enter':
+        employee[name] = True  # 직원이 사무실에 있음을 표시
+    else:
+        if name in employee:
+            del employee[name]  # 직원이 사무실을 나감
+
+# 이름을 역순으로 정렬하여 출력
+for name in sorted(employee.keys(), reverse=True):
+    print(name)
