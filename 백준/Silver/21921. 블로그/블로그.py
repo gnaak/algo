aa62a4fbbe8b4e7ad808716df6551b0e492a667a
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-02 01:27:19 +0900

    [Silver III] Title: 블로그, Time: 116 ms, Memory: 58784 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -3,24 +3,25 @@
 import sys
 input = sys.stdin.readline
 
-
-n, x = map(int,input().split())
-visited = [0] + list(map(int,input().split()))
-tot_visited = [0]*(n+1)
-tot_visited[0] = 0
-for i in range(1,n+1):
-    tot_visited[i] = tot_visited[i-1] + visited[i]
-
-max_days = 0
-cnt = 1
-for i in range(x,n+1):
-    if i >= x-1:
-        x_days = tot_visited[i] - tot_visited[i-x]
-        if x_days > max_days:
-            max_days = x_days
+def blog():
+    
+    n, x = map(int,input().split())
+    visited = [0] + list(map(int,input().split()))
+    tot_visited = [0]*(n+1)
+    tot_visited[0] = 0
+    
+    standard = sum(visited[:x])
+    max_days = standard
+    cnt = 1
+    for i in range(x,n+1):
+        standard += visited[i] - visited[i-x]
+        if max_days < standard:
+            max_days = standard
             cnt = 1
-        elif x_days == max_days:
+        elif max_days == standard:
             cnt += 1
+    
+    print(max_days if max_days > 0 else 'SAD')
+    print(cnt if max_days > 0 else '')
 
-print(max_days if max_days > 0 else "SAD")
-print(cnt if max_days > 0 else '')
+blog()
