1d7854596a4f3df86cbe2440db0078513f0c2f7e
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-04 23:49:21 +0900

    [Gold IV] Title: 부분합, Time: 96 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,16 +1,18 @@
-
-
 n, s = map(int,input().split())
 elements = list(map(int,input().split()))
 cnt = 1e9
+start = 0
 end = 0
 tot_sum = 0
-for start in range(n):
-    while tot_sum < s and end < n:
-        tot_sum += elements[end]
-        end += 1
-    if tot_sum >= s and end - start < cnt :
-        cnt = end - start
-    tot_sum -= elements[start]
+while end < n :
+    tot_sum += elements[end]
+    end += 1
+
+    while tot_sum >= s:
+        if cnt > end - start:
+            cnt = end - start
+        tot_sum -= elements[start]
+        start += 1
+
 
 print(cnt if cnt != 1e9 else 0)
