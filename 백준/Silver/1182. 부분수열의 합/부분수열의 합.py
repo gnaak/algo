b7c4413b076076e8da64f2246b753c29324740e1
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-05 00:52:18 +0900

    [Silver II] Title: 부분수열의 합, Time: 300 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,25 +1,18 @@
-'''
-백준 실버 2
-부분수열의 합
-'''
+# 부분 수열의 합
 
 import sys
 input = sys.stdin.readline
 
-n, m = map(int,input().split()) # 정수의 갯수 n, 정수 m
-arr = list(map(int,input().split()))
-cnt = 0
-ans = []
-
-def total(start):
-    global cnt
-    if sum(ans) == m and len(ans)>0:
-        cnt+=1
+def total(start, sum):
+    global ans
+    if sum == s and start > 0:
+        ans += 1
 
     for i in range(start,n):
-        ans.append(arr[i])
-        total(i+1)
-        ans.pop()
+        total(i+1, sum+arr[i])
 
-total(0)
-print(cnt)
+n, s = map(int,input().split())
+arr = list(map(int,input().split()))
+ans = 0
+total(0,0)
+print(ans)
