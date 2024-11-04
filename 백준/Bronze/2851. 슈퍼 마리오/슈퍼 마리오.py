8edc1ff5064e09f13b1360d021a3d0425df842c3
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-04 19:31:16 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -1,22 +1,14 @@
 # 슈퍼 마리오
 
-import sys
-input = sys.stdin.readline
-
-
+mush = [int(input()) for _ in range(10)]
 tot_mushroom = [0]*10
-diff = 100
-ans = 0
-for i in range(10):
-    mushroom = int(input())
-    if i == 0 :
-        tot_mushroom[0] = mushroom
-    else:   
-        tot_mushroom[i] = tot_mushroom[i-1] + mushroom
-
-    if abs(100-tot_mushroom[i]) <= diff:
+tot_mushroom[0] = mush[0]
+diff = abs(100 - mush[0])
+ans = tot_mushroom[0]
+for i in range(1,10):
+    tot_mushroom[i] = tot_mushroom[i-1] + mush[i]
+    if abs(100-tot_mushroom[i]) <= diff :
         diff = abs(100-tot_mushroom[i])
         ans = tot_mushroom[i]
 
 print(ans)
-
