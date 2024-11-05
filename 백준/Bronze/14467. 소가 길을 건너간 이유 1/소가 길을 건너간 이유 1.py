9a050045350ee050dfaa1e710a51905f923108ba
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-05 14:46:22 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,19 +1,13 @@
-# 소가 길을 건너간 이유 1
-
-# 존은 소의 위치를 n번 관찰
-# 각 관찰은 소의 번호, 소의 위치
-# 소의 번호는 1~10이고, 길의 왼쪽과 오른쪽을 뜻하는 0, 1
-
-# 소가 최소 몇번 건넜는지 확인
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-cows = dict()
+cow = {}
 cnt = 0
 for _ in range(n):
-    cow, place = input().split()
-    if cow in cows and cows[cow] != place:
-        cnt += 1
-    cows[cow] = place
-
+    num, place = map(int,input().split())
+    if num in cow and cow[num] != place:
+        cnt +=1 
+    cow[num] = place
 
 print(cnt)
