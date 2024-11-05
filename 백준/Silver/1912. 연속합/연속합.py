2d40fc9f4157f2dce16293c09821a5cae7f60253
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-05 16:13:18 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,15 +1,11 @@
-'''
-백준 실버 2
-연속합
-'''
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int,input().split()))
-
-dp = [0]* n
-dp[0] = arr[0]
-
+num = list(map(int,input().split()))
+dp = [0]*n
+dp[0] = num[0]
 for i in range(1,n):
-    dp[i] = max(arr[i], arr[i]+dp[i-1])
+    dp[i] = max(num[i],dp[i-1]+num[i])
 
 print(max(dp))
