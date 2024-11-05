16f3f9561ac6481c310680f1ed865972fad21e8e
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-05 17:16:37 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
@@ -1,29 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-stuff = list(map(int, input().split()))
-
-# 만약 책의 개수가 0인 경우
-
-start = 0
-end = 0
+n, m = map(int,input().split())
+books = list(map(int,input().split()))
+total = 0
 cnt = 0
-tot = 0
-
-if n == 0:
-    print(0)
-else:
-    while end < n:
-        # 현재 박스에 책을 계속 추가
-        if tot + stuff[end] > m:
-            cnt += 1
-            tot = stuff[end]
-        else:
-            tot += stuff[end]
-        end += 1
-    
-    if tot > 0:
+for i in range(n):
+    if total + books[i] <= m:
+        total += books[i]
+    else:
         cnt += 1
-    
-    print(cnt)
+        total = books[i]
+
+if total > 0 :
+    cnt += 1
+
+print(cnt)
