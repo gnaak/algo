258c01e4f8df6c5d9ae25db6614669967bc0c0f7
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-07 18:54:03 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,16 +1,26 @@
+# 쉬운 계단 수
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [[0]*10 for _ in range(n+1)] # 0부터 9까지는 마지막 자리수 값
-# n+1은 최종값
-for i in range(1, 10):
+mod = 1000000000
+
+dp = [[0]*10 for _ in range(n+1)]
+
+# n이 1일때, 1~9
+for i in range(1,10):
     dp[1][i] = 1
 
-for j in range(2,n+1):
-    dp[j][0] = dp[j-1][1]
-    dp[j][9] = dp[j-1][8]
-    for i in range(1,9):
-        dp[j][i] = dp[j-1][i-1] + dp[j-1][i+1]
+# n이 2 이상일 때, dp[1]의 마지막 자리수로 개수 결정됨
+for i in range(2,n+1):
+    # 0과 9는 앞자리 수가 1 or 8일때만 가능
+    dp[i][0] = dp[i-1][1]
+    dp[i][9] = dp[i-1][8]
 
-print(sum(dp[n])%1000000000)
+    # ex) 2는 앞 자리 수가 1 or 3
+    for j in range(1,9):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n])%mod)
+
