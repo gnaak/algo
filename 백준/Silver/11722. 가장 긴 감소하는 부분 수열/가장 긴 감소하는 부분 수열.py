04dfd61b4be6f96dc23b8100bf5f85fa7d352f23
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-09 20:19:48 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 140 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,18 @@
+# 가장 긴 감소하는 부분 수열
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+dp = [1]*n
+
+for i in range(1, n):
+    # 이전 값들만 비교해서,
+    for j in range(i-1,-1,-1):
+        # i가 더 작다면,
+        if a[i] < a[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+            
+
+print(max(dp))
