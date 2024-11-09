da09f2825adb75c671c2e59f7e9332172afee5fd
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-09 22:57:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,17 @@
-'''
-백준 실버 4
-설탕 배달
-'''
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-cnt = 0
 
-while n >= 0:
-    if n % 5 == 0:
-        cnt += n//5
-        print(cnt)
-        break
-    n -=3
-    cnt +=1
+dp = [float('inf')]*(n+1)
+# 무게 0을 만들 수 있는 봉지의 개수는 0개
+dp[0] = 0
 
-else:
-    print(-1)
+sugars = [3, 5]
+# 중복이 가능하니까 dp를 정순으로
+for i in range(1, n+1):
+    for sugar in sugars:
+        if i-sugar >= 0 and dp[i] > dp[i-sugar] + 1:
+            dp[i] = dp[i-sugar] +1
+
+print(dp[-1] if dp[-1] != float('inf') else -1)
