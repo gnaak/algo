941c262457b524b0c92203be9b1fd94cb05c30b2
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-11 01:12:41 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,13 +1,23 @@
+# 퇴사
+# n+1 일째 되는 날 퇴사를 하기 위해서 n일 동안 최대한 많은 상담을 하려고 함
+
 n = int(input())
-work = [list(map(int, input().split())) for _ in range(n)]
-money = [0] * (n + 1)  # n+1 크기의 DP 테이블
+work = [[0,0]] + [list(map(int,input().split())) for _ in range(n)]
+# t, p 는 완료하는데 걸리는 기간 t, 상담을 했을 때 받을 수 있는 금액 p
 
-for i in range(n):
+# 0일차부터 n일차까지 벌 수 있는 돈
+money = [0]*(n+1)
+
+for i in range(1,n+1):
     time, pay = work[i]
-    # 현재 날짜까지의 최대 수익을 다음 날들에 반영
-    if i + time <= n:  # 퇴사일을 넘지 않으면
-        money[i + time] = max(money[i + time], money[i] + pay)
-    # 다음 날의 최대 수익도 현재까지의 최대 수익으로 갱신
-    money[i + 1] = max(money[i + 1], money[i])
 
-print(money[n])
+    # 일을 했을 때, n일 전에 끝낼 수 있으면,
+    if time + i <= n+1 :
+        # 이전에 했던 일들 중에서,
+        for j in range(i-1,-1,-1):
+            # 일을 하고 나서, 일을 할 시간이 된다면, 금액을 추가
+            if i >= work[j][0] + j:
+                money[i] = max(money[j] + pay, money[i])
+
+print(max(money))
+
