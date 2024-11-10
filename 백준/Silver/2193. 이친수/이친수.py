c2b7328cc0d9ac72eda06fe1cbb3314b75e707bc
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-11 01:40:30 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,13 +1,17 @@
+# 이친수
+# 이친수는 0으로 시작하지 않는다.
+# 이친수는 1이 연속해서 나오지 않는다.
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+dp = [0]*(n+1)
 
-# n 자리 수 만큼 dp 테이블 생성 => n+1
-dp = [0] * (n + 1)
-dp[1] = 1
+if n == 1 or n ==2:
+    print(1)
+else:
+    dp[1],dp[2] = 1, 1
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
 
-for i in range(2, n+1):
-    dp[i] = dp[i-2] + dp[i-1]
-
-print(dp[n])
+    print(dp[n])
