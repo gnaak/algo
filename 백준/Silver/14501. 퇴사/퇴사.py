3fe77419edcd102bf18eaf993ee468e9fbb89359
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-11 01:05:19 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,21 +1,13 @@
-'''
-백준 실버 3
-퇴사
-'''
-
 n = int(input())
-list = []
-for _ in range(n):
-    a, b = map(int,input().split())
-    list.append([a,b])
+work = [list(map(int, input().split())) for _ in range(n)]
+money = [0] * (n + 1)  # n+1 크기의 DP 테이블
 
-dp = [0]*(n+1)
+for i in range(n):
+    time, pay = work[i]
+    # 현재 날짜까지의 최대 수익을 다음 날들에 반영
+    if i + time <= n:  # 퇴사일을 넘지 않으면
+        money[i + time] = max(money[i + time], money[i] + pay)
+    # 다음 날의 최대 수익도 현재까지의 최대 수익으로 갱신
+    money[i + 1] = max(money[i + 1], money[i])
 
-# top-down
-for i in range(n-1, -1, -1):
-    if i+list[i][0] > n :
-        dp[i] = dp[i+1] # 상담 진행 x
-    else:
-        dp[i] = max(dp[i+1], list[i][1] + dp[i+list[i][0]])
-
-print(dp[0])
+print(money[n])
