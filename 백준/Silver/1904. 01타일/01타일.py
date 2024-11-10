82d92c075f903f2a13cdffda755acf06ff5118c3
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-11 02:38:19 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,25 +1,21 @@
-def count_num(n) :
-    MOD = 15746
-    
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    
-    else:
-        prev2 = 1
-        prev1 = 2
-        current = 0
-
-        for i in range(3, n+1):
-            current = (prev2 + prev1) % 15746
-            prev2 = prev1
-            prev1 = current
-            
-    return current 
 
 import sys
 input = sys.stdin.readline
 
+def calc(n):
+    mod = 15746
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        a,b = 1, 2
+        for i in range(n-2):
+            a, b = b, (a + b) % mod
+        return b
+
 n = int(input())
-print(count_num(n))
+print(calc(n))
+
+
