df82829fe9cd2224d16c45be57b2740b09ba4f69
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-15 17:34:49 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,26 +1,22 @@
+# 가장 긴 증가하는 부분 수열 4
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int, input().split()))
-dp = [1] * n
+arr = list(map(int,input().split()))
+dp = [1]*n
 
-# DP 배열 계산
-for i in range(1, n):
+for i in range(1,n):
     for j in range(i):
         if arr[i] > arr[j] and dp[j] >= dp[i]:
             dp[i] = dp[j] + 1
 
-# LIS 길이 출력
 k = max(dp)
 print(k)
-
-# LIS 수열 역추적
 ans = []
-for i in range(n - 1, -1, -1):
+for i in range(n-1,-1,-1):
     if dp[i] == k:
         ans.append(arr[i])
         k -= 1
-
-# 결과 출력
-print(' '.join(map(str, ans[::-1])))
+print(' '.join(map(str,ans[::-1])))
