40edcd4653db613bfed323400e009e2c1e05ca14
이근학 <139318007+gnaak@users.noreply.github.com>
2024-11-21 00:35:50 +0900

    [Gold V] Title: 제곱수 찾기, Time: 76 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.py
@@ -1,27 +1,29 @@
-from math import sqrt
-n, m = map(int, input().split())
+# 제곱수 찾기
 
-arr = [list(map(int, input())) for _ in range(n)]
-result = -1
-def is_sqrt(x):
-    if int(sqrt(int(x))) ** 2 == int(x):
+import sys
+input = sys.stdin.readline
+from math import sqrt
+def check(tmp):
+    if int(sqrt(int(tmp)))**2 == int(tmp):
         return True
     else:
         return False
+n, m = map(int,input().split())
+board = [list(map(int,input().strip())) for _ in range(n)]
 
+ans = -1
 for i in range(n):
     for j in range(m):
-        for diff_x in range(-n, n):
-            for diff_y in range(-m, m):
+        for diff_y in range(-n, n):
+            for diff_x in range(-m, m):
+                y, x = i, j
                 tmp = ''
-                x, y = i, j
-                if diff_x == 0 and diff_y == 0:
+                if diff_y ==0 and diff_x == 0:
                     continue
-                while 0 <= x < n and 0 <= y < m:
-                    tmp += str(arr[x][y])
-                    if is_sqrt(tmp):
-                        result = max(result, int(tmp))
-                    x += diff_x
+                while 0<=y<n and 0<=x<m:
+                    tmp += str(board[y][x])
+                    if check(tmp):
+                        ans = max(ans, int(tmp))
                     y += diff_y
-
-print(result)
+                    x += diff_x
+print(ans)
